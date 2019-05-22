a035996ac92eec411bf810b7a5b4db390ca0c4ec
Administrator <Administrator@MSDN-SPECIAL>
2019-05-22 14:34:21 +0900

    웹소설 플랫폼 프로젝트 초기화

---
+++ wnp/src/main/java/cafe/jjdev/ajaxtest/WnpApplication.java
@@ -0,0 +1,13 @@
+package cafe.jjdev.ajaxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WnpApplication.class, args);
+	}
+
+}
